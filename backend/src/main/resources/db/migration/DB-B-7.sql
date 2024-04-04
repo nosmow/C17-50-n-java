e0196926e4caade340ff0726b1e75010f5d374cd
Cristian Solís <cristiansolis.it@gmail.com>
2024-04-04 00:27:29 -0300

    feat(DB-B-7): create users table

---
+++ backend/src/main/resources/db/migration/DB-B-7.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `digital_bank`.`users`
+(
+    `id`       INT UNSIGNED                                      NOT NULL AUTO_INCREMENT,
+    `role`     ENUM ('ROLE_ADMIN', 'ROLE_USER', 'ROLE_BUSINESS') NOT NULL,
+    `email`    VARCHAR(255)                                      NOT NULL UNIQUE,
+    `phone`    INT                                               NULL,
+    `country`  VARCHAR(100)                                      NOT NULL,
+    `password` VARCHAR(255)                                      NOT NULL,
+    `active`   TINYINT(1)                                        NOT NULL,
+    PRIMARY KEY (`id`)
+);
