7e838e80ed79f8259d3763c1fd9b20dcefa23327
nosmow <nosmow.dev@gmail.com>
2024-04-12 17:13:52 -0500

    feat(DB-B-32): Create the script for the companies table

---
+++ backend/src/main/resources/db/migration/DB-B-32.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `digital_bank`.`companies`
+(
+    `id`       INT UNSIGNED                                      NOT NULL AUTO_INCREMENT,
+    `user_id`  INT UNSIGNED                                      NOT NULL,
+    `company`     VARCHAR(100)                                   NOT NULL,
+    `cuit` VARCHAR(11)                                           NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `digital_bank`.`users`(`id`)
+);
