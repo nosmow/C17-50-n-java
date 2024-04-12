01088594e6aa626c7c77fd030411b1e98f2acb0d
nosmow <nosmow.dev@gmail.com>
2024-04-12 17:02:54 -0500

    feat(DB-B-31): Create the script for the people table

---
+++ backend/src/main/resources/db/migration/DB-B-31.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `digital_bank`.`people`
+(
+    `id`       INT UNSIGNED                                      NOT NULL AUTO_INCREMENT,
+    `user_id`  INT UNSIGNED                                      NOT NULL,
+    `name`     VARCHAR(100)                                      NOT NULL,
+    `lastname` VARCHAR(100)                                      NOT NULL,
+    `dni`      VARCHAR(9)                                        NOT NULL UNIQUE,
+    `birthdate`DATE                                              NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `digital_bank`.`users`(`id`)
+);
